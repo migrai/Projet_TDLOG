1389fceceb7c609cf814759071a1a884d28db6b5
Antoine Massieu <antoine.massieu@eleves.enpc.fr>
2023-11-06 14:51:32 +0100

    crétion player

---
+++ player.py
@@ -0,0 +1,26 @@
+class Player:
+    """A player have :
+      a colour
+      a attribute if it is his turn to play 
+    """
+    def __init__(self
+                 color):
+        self._color = color 
+
+    @property
+    def color(self):
+        return self._color
+    
+class Humanplayer(Player):
+    """ A human player has one extra attribute, their name. 
+    they will choose their move using the mouse"""
+    def __init__(self,name,color):
+        assert name.strip() != "", "invalid name"      
+        super().__init__(color)
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+
