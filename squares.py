8b9a872398b59a4c1ebec1b7bb0c261aa8783749
Antoine Massieu <antoine.massieu@eleves.enpc.fr>
2023-11-06 15:37:24 +0100

    v3 square

--- squares.py
+++ squares.py
@@ -1,22 +1,32 @@
 class Square:
-    def __init__(self):
-        self.state = "unoccupied"  # L'état initial est inoccupé
+    def __init__(self,
+                 state,
+                 colorinside, 
+                 coloroutline):
+        self._state = self  
+        self._colorinside = colorinside     #color of the square
+        self._coloroutline = coloroutline   #color of the outline of the square
 
+    @property
+    def coloroutline(self):
+        return self._coloroutline
+    
+    @property
+    def colorinside(self):
+        return self._colorinside
+    
+    @property
+    def state(self):
+        return self._state
+    
 
 class SmallSquare(Square):
-    def __init__(self):
-        self.state = "unoccupied"  # L'état initial est inoccupé
-
-    def occupy(self, color):
-        self.state = "occupied_" + color  # "occupied_blue" ou "occupied_red"
+    def __init__(self, state, colorinside, coloroutline):
+        super().__init__(state, colorinside, coloroutline)
+        
 
 
 class BigSquare(Square):
-    def __init__(self):
-        self.state = "unoccupied"  # L'état initial est inoccupé
+    def __init__(self, state, colorinside, coloroutline):
+        super().__init__(state, colorinside, coloroutline)
 
-    def occupy(self, color):
-        self.state = "occupied_" + color  # "occupied_blue" ou "occupied_red"
-    
-    def next_move(self, color):
-        self.state = 
