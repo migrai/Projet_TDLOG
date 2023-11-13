97e046959c585205c57e77f3681b038fb5517028
THEDIM SAYAO Miguel <miguel.thedim@gmail.com>
2023-11-13 16:07:36 +0100

    13/11/2023 - Elaborated board with different colors, formatted.

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
-from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QSizePolicy
+
 
 class Board(QWidget):
     def __init__(self):
@@ -12,25 +12,29 @@ class Board(QWidget):
         self.setLayout(grid)
 
         # Create the matrix to represent the board
-        self.tabuleiro = [[None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],
-                          [None, None, None, None, None, None, None, None, None],]
+        self.tabuleiro = [
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+        ]
 
         # Variable to track the current player (X or O)
-        self.current_player = 'X'
+        self.current_player = "X"
 
         # Create the buttons and add them to the matrix
         for i in range(9):
             for j in range(9):
-                btn = QPushButton('', self)
+                btn = QPushButton("", self)
+                btn.setSizePolicy(
+                    QSizePolicy.Expanding, QSizePolicy.Expanding
+                )  # Set size policy
                 btn.clicked.connect(lambda _, row=i, col=j: self.button_click(row, col))
-                btn.setStyleSheet("background-color: white; border: 1px solid black")
                 grid.addWidget(btn, i, j)
                 self.tabuleiro[i][j] = btn
 
@@ -39,28 +43,36 @@ class Board(QWidget):
                 font.setPointSize(20)
                 btn.setFont(font)
 
-                # Set the minimum and maximum size to ensure the button is square
-                min_size = min(btn.sizeHint().width(), btn.sizeHint().height())
-                btn.setMinimumSize(min_size, min_size)
-                btn.setMaximumSize(min_size, min_size)
+                # Set background color based on the block position
+                block_color = self.get_block_color(i // 3, j // 3)
+                btn.setStyleSheet(
+                    f"background-color: {block_color}; border: 1px solid black"
+                )
 
-        self.setWindowTitle('Meta-Morpion')
+        self.setWindowTitle("Meta-Morpion")
         self.show()
 
     def button_click(self, row, col):
         # Function to be called as the button is clicked
         if not self.tabuleiro[row][col].text():  # Check if the button is empty
             self.tabuleiro[row][col].setText(self.current_player)
-            self.tabuleiro[row][col].setStyleSheet(f'background-color: {self.get_player_color()}')
+            self.tabuleiro[row][col].setStyleSheet(
+                f"background-color: {self.get_player_color()}"
+            )
 
             # Toggle player
-            self.current_player = 'O' if self.current_player == 'X' else 'X'
+            self.current_player = "O" if self.current_player == "X" else "X"
 
     def get_player_color(self):
         # Return color based on the current player
-        return 'red' if self.current_player == 'X' else 'blue'
+        return "red" if self.current_player == "X" else "blue"
 
-if __name__ == '__main__':
+    def get_block_color(self, row, col):
+        # Return alternating colors for the blocks
+        return "lightgray" if (row + col) % 2 == 0 else "gray"
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     tabuleiro = Board()
     sys.exit(app.exec_())
