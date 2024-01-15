83f5d05a39557534892eb5dd5f50660e7a785cfb
THEDIM SAYAO Miguel <miguel.thedim@gmail.com>
2024-01-15 14:24:33 +0100

    another one

--- gamewindow.py
+++ gamewindow.py
@@ -1,5 +1,5 @@
 from board import *
-from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QDesktopWidget, QLabel
 from PyQt5.QtGui import QIcon
 
 class GameWindow(QMainWindow):
@@ -22,11 +22,23 @@ class GameWindow(QMainWindow):
         return_to_main_menu.triggered.connect(lambda: self.show_main_menu(ui))
         match_menu.addAction(return_to_main_menu)
 
+        # Set up the scoreboard
+        self.scoreboard = Scoreboard()
+
         self.central_widget = Board()
-        self.setCentralWidget(self.central_widget)
+
+        # Set the layout for the main window
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.scoreboard)
+        main_layout.addWidget(self.central_widget)
+        central_widget_container = QWidget()
+        central_widget_container.setLayout(main_layout)
+        self.setCentralWidget(central_widget_container)
 
         self.setGeometry(900, 900, 900, 900)
         self.setWindowTitle('Meta-Morpion')
+
+        self.center()
 
     def show_main_menu(self, MainWindow):
         original_geometry = MainWindow.save_geometry()
@@ -42,3 +54,26 @@ class GameWindow(QMainWindow):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+class Scoreboard(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.score_label = QLabel('Score: 0', self)
+        self.game_label = QLabel('Game Status', self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.score_label, alignment=Qt.AlignTop | Qt.AlignRight)
+        vbox.addWidget(self.game_label, alignment=Qt.AlignTop | Qt.AlignRight)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+    def update_score(self, score):
+        self.score_label.setText(f'Score: {score}')
+
+    def update_game_label(self, text):
+        self.game_label.setText(text)
